a8d9e682bab014633c9ab19b32704719cb1204c3
bohdan_lyhoshapko <bohdan09012000@gmail.com>
2021-04-16 23:18:39 +0300

    Mediator and Observer design pattern

---
+++ behavior_patterns/14_observer.js
@@ -0,0 +1,57 @@
+// в observers - ми можемо зберігати звичайні callback функції. І також можемо зберігати обекти певного класу.
+class Subject {
+    constructor() {
+        this.observers = [];
+    }
+
+    subscribe(observer) {
+        this.observers.push(observer);
+    }
+
+    unsubscribe(observer) {
+        this.observers = this.observers.filter(obs => obs !== observer);
+    }
+
+    fire(action) {
+        this.observers.forEach(observer => {
+            observer.update(action);
+        });
+    }
+}
+
+class Observer {
+    constructor(state = 1) {
+        this.state = state;
+        this.initialState = state;
+    }
+
+    update(action) {
+        switch(action.type) {
+            case 'INCREMENT' : 
+                this.state = ++this.state;
+                break;
+            case 'DECREMENT' :
+                this.state = --this.state;
+                break;
+            case 'ADD' :
+                this.state += action.payload;
+                break;
+            default: 
+                this.state = this.initialState;
+        }
+    }
+}
+
+
+const stream$ = new Subject();
+const obs1 = new Observer();
+const obs2 = new Observer(42);
+
+stream$.subscribe(obs1);
+stream$.subscribe(obs2);
+
+// ми можемо трігирити одне собитие або всі собития в observers. 
+stream$.fire({type: 'INCREMENT'});
+
+console.log(obs1.state);
+console.log(obs2.state);
